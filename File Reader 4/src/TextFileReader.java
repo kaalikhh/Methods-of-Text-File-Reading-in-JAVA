1cf8a2f07f197210e46177bad3a217251d9c8036
Saurabh Yadav <saurabhy789@gmail.com>
2018-07-05 14:14:47 +0200

    TextFileReader
    I've added the choice for reading lines. User can manually enter the number of lines he wants to read from the text file.

--- File Reader 4/src/TextFileReader.java
+++ File Reader 4/src/TextFileReader.java
@@ -1,4 +1,36 @@
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 public class TextFileReader {
 
+	public static void main(String[] args) throws Exception
+		 {
+		    // pass the path
+		    try {
+		    	FileReader test = new FileReader("Enter the path of the text file");
+		    	BufferedReader reader = new BufferedReader(test);		
+		    	System.out.println("How many lines you want to read: ");
+		    	Scanner s = new Scanner(System.in); 
+		    	int k =1;
+		    	int n = s.nextInt();
+		    	while (true){
+		    		if (! reader.ready()) 
+		    			break;
+		    		String next_line = reader.readLine();
+		    		  if (k<=n){
+		    		    System.out.println("Line Number: " +k+ " - " + next_line);
+		    		    }
+		    		  k++;
+		    	  }
+		    	reader.close();
+		    	s.close();
+		    }
+		    catch (FileNotFoundException ex) {
+		        ex.printStackTrace();
+		    } catch (IOException ex) {
+		        ex.printStackTrace();
+		    }
+		 }
 }
